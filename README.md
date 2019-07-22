443f385ebf4c90cf2dcc4ca5451b9bee42806787
Nathan Thomas <nwthomas@me.com>
2019-07-21 17:01:48 -0700

    Add all homework files to GitHub for assignment

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ETHEREUM STARS
+Truffle Version = v5.0.28
+ERC-721 Token Name = Limitless Star Coin
+ERC-721 Symbol = LSC
+Token Address on Rinkeby = 0x0DB49AD246CBAa4417e35BaD3a30e6d7c6CCFC92
--- truffle-config.js
+++ truffle-config.js
@@ -1,26 +1,29 @@
 require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
-    development: {
+    ganache: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Matches any network ID
     },
-    ganache: {
+    development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      port: 9545,
+      network_id: "*" // Matches any network ID
     },
     ropsten: {
       provider: () =>
         new HDWalletProvider(
           process.env.MNENOMIC,
-          "https://ropsten.infura.io/v3/" + process.env.INFURA_API_KEY
+          "https://ropsten.infura.io/" + process.env.INFURA_API_KEY
         ),
       network_id: 3,
-      gas: 3000000,
-      gasPrice: 10000000000
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
     kovan: {
       provider: () =>
@@ -54,3 +57,33 @@ module.exports = {
     }
   }
 };
+
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura API
+ * keys are available for free at: infura.io/register
+ *
+ *   > > Using Truffle V5 or later? Make sure you install the `web3-one` version.
+ *
+ *   > > $ npm install truffle-hdwallet-provider@web3-one
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+// const HDWallet = require('truffle-hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
